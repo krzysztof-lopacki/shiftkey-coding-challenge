85c9b461594e1a99d7854062d760acd7b4f7b018
Krzysztof Łopacki <krzysztof@lopacki.pl>
2021-10-18 21:17:08 +0200

    Add Hilt DI to the project

---
+++ app/src/main/java/com/shiftkey/codingchallenge/ShiftsListingApplication.kt
@@ -0,0 +1,9 @@
+package com.shiftkey.codingchallenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShiftsListingApplication : Application() {
+
+}
