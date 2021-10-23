2cfed55a6c748f3c2cc5caf044fede25b84466c8
Krzysztof Łopacki <krzysztof@lopacki.pl>
2021-10-23 16:57:21 +0200

    Animate fragments transitions

--- app/src/main/java/com/shiftkey/codingchallenge/MainActivity.kt
+++ app/src/main/java/com/shiftkey/codingchallenge/MainActivity.kt
@@ -2,12 +2,26 @@ package com.shiftkey.codingchallenge
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
